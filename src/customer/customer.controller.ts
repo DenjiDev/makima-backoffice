095ff0f7c554c3390e3dab37e7d54d631a8c3e93
allan-silva-intuitive <allan.silva@intuitivecare.com>
2023-07-04 18:22:29 -0300

    feat/MB-72 - ID que estava como string passado como parametro

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -29,7 +29,7 @@ export class CustomerController {
   @ApiResponse({ status: HttpStatus.OK, description: 'The customer has been returned successfully.', type: [GetCustomerDto] })
   @Get(":id")
   findOne(@Param('id') id:string) {
-    return this.customerService.findOne('id')
+    return this.customerService.findOne(id)
   }
 
 }
