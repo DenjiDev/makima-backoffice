b4c73614578be5b43166bcc1ce532cae106180bb
Edison Matos <edison.matos@live.com>
2023-07-10 14:04:09 -0300

    feat(MB-64 & MB-63): Create premium GROUPs routes with POST and GET methods.

---
+++ src/group/dto/get-customer.dto.ts
@@ -0,0 +1,3 @@
+import { Group } from "../entities/group.entity";
+
+export class GetCustomerDto extends Group{}
---
+++ src/group/group.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
+import { GroupService } from './group.service';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+
+@Controller('groups')
+export class GroupController {
+  constructor(private readonly groupService: GroupService) {}
+
+  @ApiBody({ type: CreateGroupDto })
+  @ApiOperation({ summary: 'Create group' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'The group has been created successfully.', type: CreateGroupDto })
+  @Post()
+  create(@Body() createGroupDto: CreateGroupDto) {
+    return this.groupService.create(createGroupDto);
+  }
+
+  @ApiOperation({ summary: 'List all groups'})
+  @ApiResponse({ status: HttpStatus.OK, description: 'The group list has been returned successfully.'})
+  @Get()
+  findAll() {
+    return this.groupService.findAll();
+  }
+}
